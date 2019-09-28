da471fa900e4124e96fb956840b6782b5ae167a6
MJoshua25 <46876900+MJoshua25@users.noreply.github.com>
2019-09-28 15:38:17 +0000

    ajout de categorie blog
    ajout de categorie blog

--- projet/blog_app/views.py
+++ projet/blog_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def blog(request):
     return render(request, 'pages/blog.html')
+
+def categorie(request):
+    return render(request, 'pages/blogcategorie.html')
